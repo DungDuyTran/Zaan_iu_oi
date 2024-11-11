ef5107d071bfbe4034c16fe0e7f77fdaf0e2c88c
Dum iu oi <dungduytran2004@gmail.com>
2024-11-11 11:43:05 +0700

    first commit

---
+++ Hocjs/index.js
@@ -0,0 +1,22 @@
+console.log("Hello world!");
+/*
+let name = "Hoang";
+
+let Name = "Dung";
+
+console.log("name: " + name + ",Name: " + Name);
+
+let x = 10;
+let y = 20;
+console.log("x: " + x);
+console.log("y: " + y);
+*/
+let x = 10;
+console.log(x ** 5);
+if (1 == 1) {
+  let z = 123;
+}
+console.log("z: " + z);
+
+const PI = 3.1416;
+PI = 55;
